bef823aa75340a0390f82d4f8ea031a108cd2351
katearzate <katearzate000@gmail.com>
2021-07-13 04:14:46 -0500

    mark in current location

--- app/src/main/java/com/example/findtaste/MapsActivity.kt
+++ app/src/main/java/com/example/findtaste/MapsActivity.kt
@@ -16,37 +16,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    private var lat: Double = 0.0
+    private var lng: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.mapsBtnClose.setOnClickListener {
-            finish()
-        }
+        var type = intent.getStringExtra("type")
+        lat = intent.getDoubleExtra("lat", 0.0)
+        lng = intent.getDoubleExtra("lng", 0.0)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val zoomLevel = 11f
 
-        // Add a marker in Sydney and move the camera
-        val morelia = LatLng(19.7, -101.2)
-        mMap.addMarker(MarkerOptions().position(morelia).title("Marca en Morelia!"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(morelia))
+        val current = LatLng(lat, lng)
+        mMap.addMarker(MarkerOptions().position(current).title("Tú"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, zoomLevel))
     }
 
 }
