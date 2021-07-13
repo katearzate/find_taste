4aea929a71dd5c843a07ce27ee33be379cc3f725
katearzate <katearzate000@gmail.com>
2021-07-12 23:15:33 -0500

    firebase implemented to login

--- app/src/main/java/com/example/findtaste/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/findtaste/fragments/SearchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,8 @@ import com.example.findtaste.R
 import com.example.findtaste.adapters.RecyclerFoodTypes
 import com.example.findtaste.databinding.FragmentSearchBinding
 import com.example.findtaste.models.Menu
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
 
 class SearchFragment : Fragment() {
 
@@ -41,6 +44,29 @@ class SearchFragment : Fragment() {
         menus.add(Menu(R.drawable.salad, "Vegetariana"))
         menus.add(Menu(R.drawable.dessert, "Postres"))
         menus.add(Menu(R.drawable.cafe, "Cafe"))
+
+        /*val db = FirebaseFirestore.getInstance()
+
+        val docRef = db.collection("MyDB").document("FirebaseData")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    //Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                    val imageViewSearchIcons = document.getString("SearchIcons")
+                    val textSearchIcons = document.getString("SearchIconName")
+
+                    Picasso.get().load(imageViewSearchIcons).into(textSearchIcons)
+                    menus.add(Menu(R.drawable.sushi, textSearchIcons))
+
+                } else {
+                    //Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                //Log.d(TAG, "get failed with ", exception)
+                Toast.makeText(this, "Error: $exception", Toast.LENGTH_LONG).show()
+            }
+*/
 
         binding.recyclerFoodTypes.layoutManager = GridLayoutManager(
                 requireContext(),
